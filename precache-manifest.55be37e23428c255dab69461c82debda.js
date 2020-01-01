9362957670b348fa9202b9884ebd1e4e0a505fa4
Justyna Olszewka <wioza@wp.pl>
2020-01-01 21:53:59 +0100

    Updates

---
+++ precache-manifest.55be37e23428c255dab69461c82debda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebce2750234166d25d75c0010f1571b1",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "b09363413548c2b52cc9",
+    "url": "/ToDoApp/static/css/main.8e301e0c.chunk.css"
+  },
+  {
+    "revision": "4a52e3466279406aa5e8",
+    "url": "/ToDoApp/static/js/2.a0810c85.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDoApp/static/js/2.a0810c85.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b09363413548c2b52cc9",
+    "url": "/ToDoApp/static/js/main.f3260f43.chunk.js"
+  },
+  {
+    "revision": "685016f317235ade0101",
+    "url": "/ToDoApp/static/js/runtime-main.30f04182.js"
+  }
+]);
